7db393106a0412aa1dd95719aca167f39cc7991f
00anp <alfonso.nunez29@gmail.com>
2021-08-02 22:14:14 -0500

    deployed app

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: "Amusectionary",
+    subtitle: "HTML/CSS/JS + API's",
+    description:
+      "This project was developed by: Regina Franco, Alfonso Núñez and Víctor Vallejo. The goal of our project was to find the definition of a an entered word and find a song with that word in the title.",
+    image: "../img/Amusectionary-Homepage.png",
+    link: "https://reginaafc.github.io/amusectionary/",
+  },
+  {
+    title: "TuTiendita",
+    subtitle: "Node/Express/MySQL",
+    description:
+      "This project was developed by: Alsynoé Arista, Érick Contreras, Alfonso Núñez and Sandra Pérez. The goal was to create an app that can serve as a Store Manager, as well as a Store depending on the credentials.",
+    image: "../img/tutiendita.png",
+    link: "https://desolate-shore-00305.herokuapp.com/",
+  },
+  {
+    title: "Weather Dashboard",
+    subtitle: "HTML/CSS/JS + API",
+    description:
+      "This is weather dashboard that works with the openWeather API, to get real time data for current weather and forecasts. The design of the app is vertical to make it a mobile-first application.",
+    image: "../img/WeatherDashboard-ScreenCapture.png",
+    link: "https://00anp.github.io/Weather-Dashboard/",
+  },
+  {
+    title: "Express Note Taker",
+    subtitle: "Node/Express",
+    description:
+      "This note taker app uses a server to create, store and delete notes. Your notes will be more organized, with this easy to use app. The explicit icons save any complications to understand and continue with your busy day.",
+    image: "../img/ExpressNoteTaker.png",
+    link: "https://arcane-falls-88442.herokuapp.com/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Mongo",
+  "Node",
+  "GraphQL",
+  "MySQL",
+];
